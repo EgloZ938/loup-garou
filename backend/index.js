8f71b4f65edefa2dfdfd3ae5b2f75bf6d3aee32f
EgloZ938 <theo.evon.2004@gmail.com>
2024-12-12 16:07:45 +0100

    création, connexion room. ( vue js et node )

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:5173", // URL de votre app Vue.js
+        methods: ["GET", "POST"]
+    }
+});
+
+const rooms = new Map(); // Stockage des rooms et leurs utilisateurs
+
+io.on('connection', (socket) => {
+    console.log('Un utilisateur s\'est connecté');
+
+    socket.on('createRoom', ({ username }) => {
+        const roomCode = Math.random().toString(36).substring(7);
+        rooms.set(roomCode, new Set([username]));
+        socket.join(roomCode);
+        socket.emit('roomCreated', roomCode);
+        io.to(roomCode).emit('userList', Array.from(rooms.get(roomCode)));
+    });
+
+    socket.on('joinRoom', ({ username, room }) => {
+        if (rooms.has(room)) {
+            rooms.get(room).add(username);
+            socket.join(room);
+            io.to(room).emit('userList', Array.from(rooms.get(room)));
+        }
+    });
+
+    socket.on('leaveRoom', ({ username, room }) => {
+        if (rooms.has(room)) {
+            rooms.get(room).delete(username);
+            socket.leave(room);
+
+            if (rooms.get(room).size === 0) {
+                rooms.delete(room);
+            } else {
+                io.to(room).emit('userList', Array.from(rooms.get(room)));
+            }
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Un utilisateur s\'est déconnecté');
+    });
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+    console.log(`Serveur démarré sur le port ${PORT}`);
+});
